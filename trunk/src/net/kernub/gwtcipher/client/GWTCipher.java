2c5737d4ed59b3d9a08b17247e050a5703cabd0f
jlchasseriau <jlchasseriau@035ed1fe-88c1-02ad-ca3c-0e5e38801070>
2010-09-06 10:19:16 +0000

    Make it clean

--- trunk/src/net/kernub/gwtcipher/client/GWTCipher.java
+++ trunk/src/net/kernub/gwtcipher/client/GWTCipher.java
@@ -2,8 +2,11 @@ package net.kernub.gwtcipher.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.i18n.client.Constants;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
@@ -24,23 +27,94 @@ import net.kernub.gwtcipher.client.provider.AES;
 
 public class GWTCipher implements EntryPoint {
 
-	/*
-    final DialogBox dialogBox = new DialogBox();
-    dialogBox.setText("Remote Procedure Call");
-    dialogBox.setAnimationEnabled(true);
-    dialogBox.setWidget(dialogVPanel);
-        dialogBox.hide();
-        dialogBox.center();
-	*/
+	public interface Strings extends Constants
+	{
+		public static final Strings inst = GWT.create( Strings.class );
+
+		@DefaultStringValue( "Ok" )
+		String infoBoxOkButton();
+
+		@DefaultStringValue( "Info" )
+		String infoBoxCaption();
+
+		@DefaultStringValue( "Password" )
+		String password();
+
+		@DefaultStringValue( "Confirm password" )
+		String confirmPassword();
+
+		@DefaultStringValue( "Input" )
+		String input();
+
+		@DefaultStringValue( "Output" )
+		String output();
+
+		@DefaultStringValue( "Clear" )
+		String clearButton();
+
+		@DefaultStringValue( "Encrypt" )
+		String encryptButton();
+
+		@DefaultStringValue( "Decrypt" )
+		String decryptButton();
+
+		// Should be a message with exception message added
+		@DefaultStringValue( "Passwords does not match." )
+		String errorPasswordNotMatch();
+
+		@DefaultStringValue( "Passwords format error." )
+		String errorPasswordFormat();
+
+		@DefaultStringValue( "Error while encrypting." )
+		String errorEncrypting();
+
+		@DefaultStringValue( "Error while decrypting.<br>\nIs it an encrypted input?" )
+		String errorDecrypting();
+	};
+
+	private class InfoBox extends DialogBox
+	{
+		private final HTML infoHTML = new HTML();
+
+		public InfoBox()
+		{
+			infoHTML.addStyleName( "GWTCipher_InfoBox_infoHTML" );
+
+			final Button okButton = new Button( Strings.inst.infoBoxOkButton() );
+			okButton.addStyleName( "GWTCipher_InfoBox_okButton" );
+			okButton.addClickHandler( new ClickHandler() {
+				public void onClick( ClickEvent event ) {
+					hide();
+				}
+			});
+
+			final FlowPanel panel = new FlowPanel();
+			panel.add( infoHTML );
+			panel.add( okButton );
+
+			setText( Strings.inst.infoBoxCaption() );
+			setAnimationEnabled( true );
+			setGlassEnabled( true );
+			setWidget( panel );
+		}
+
+		public void showInfo( final String txt )
+		{
+			infoHTML.setHTML( txt );
+			center();
+		}
+	};
 
   public void onModuleLoad() {
 		final AES aes = new AES();
 
-		final Label passwordLabel = new Label( "Password (if <16,24,32 it is padded with white spaces)" );
-		final Label inputLabel = new Label( "Input" );
-		final Label outputLabel = new Label( "Output" );
-		final PasswordTextBox passwordTextBox = new PasswordTextBox();
-		// todo: add changeHandler to detect when it has change and avoid calling aes "setKey()" each time
+		final InfoBox infoBox = new InfoBox();
+		final Label passwordLabel = new Label( Strings.inst.password() );
+		final Label confirmPasswordLabel = new Label( Strings.inst.confirmPassword() );
+		final Label inputLabel = new Label( Strings.inst.input() );
+		final Label outputLabel = new Label( Strings.inst.output() );
+		final PasswordTextBox passwordTextBox1 = new PasswordTextBox();
+		final PasswordTextBox passwordTextBox2 = new PasswordTextBox();
 
 		final TextArea area1 = new TextArea();
 		area1.setVisibleLines( 15 );
@@ -52,17 +126,19 @@ public class GWTCipher implements EntryPoint {
 		area2.setCharacterWidth( 80 );
 		area2.setReadOnly( true );
 
-		final Button clearButton = new Button( "Clear" );
+		final Button clearButton = new Button( Strings.inst.clearButton() );
 		clearButton.addStyleName( "GWTCipher_button" );
-		final Button encryptButton = new Button( "Encrypt" );
+		final Button encryptButton = new Button( Strings.inst.encryptButton() );
 		encryptButton.addStyleName( "GWTCipher_button" );
-		final Button decryptButton = new Button( "Decrypt" );
+		final Button decryptButton = new Button( Strings.inst.decryptButton() );
 		decryptButton.addStyleName( "GWTCipher_button" );
 
 		final FlowPanel passwordPanel = new FlowPanel();
 		passwordPanel.addStyleName( "GWTCipher_passwordPanel" );
 		passwordPanel.add( passwordLabel );
-		passwordPanel.add( passwordTextBox );
+		passwordPanel.add( passwordTextBox1 );
+		passwordPanel.add( confirmPasswordLabel );
+		passwordPanel.add( passwordTextBox2 );
 
 		final FlowPanel buttonPanel = new FlowPanel();
 		buttonPanel.addStyleName( "GWTCipher_buttonPanel" );
@@ -79,39 +155,64 @@ public class GWTCipher implements EntryPoint {
 		panel.add( outputLabel );
 		panel.add( area2 );
 
-		class Handlers implements ClickHandler
+		class Handlers implements ClickHandler, ChangeHandler
 		{
+			private boolean passwordChanged = false;
+
+			public void onChange( ChangeEvent event )
+			{
+				if( event.getSource() == passwordTextBox1 || event.getSource() == passwordTextBox2 )
+				{
+					passwordChanged = true;
+				}
+			}
+
+			private boolean updateKey()
+			{
+				if( !passwordChanged ) return true;
+
+				if( !passwordTextBox1.getText().equals( passwordTextBox2.getText() ) ) {
+					infoBox.showInfo( Strings.inst.errorPasswordNotMatch() );
+					return false;
+				}
+
+				try {
+					aes.setKey( passwordTextBox1.getText() );
+				} catch( Exception e ) {
+					infoBox.showInfo( Strings.inst.errorPasswordFormat() );
+					return false;
+				}
+
+				passwordChanged = false;
+				return true;
+			}
+
 			public void onClick( ClickEvent event )
 			{
 				if( event.getSource() == encryptButton )
 				{
 					area2.setText( "" );
+					if( !updateKey() ) return;
 					try {
-						aes.setKey( passwordTextBox.getText() );
+						area2.setText( aes.encryptBase64( area1.getText() ) );
 					} catch( Exception e ) {
-						// todo: replace with dialogBox
-						Window.alert( "Password format mistmatch:\n" + e.getMessage() );
-						return;
+						infoBox.showInfo( Strings.inst.errorEncrypting() );
 					}
-
-					area2.setText( aes.encryptBase64( area1.getText() ) );
 				}
 				else if( event.getSource() == decryptButton )
 				{
 					area2.setText( "" );
+					if( !updateKey() ) return;
 					try {
-						aes.setKey( passwordTextBox.getText() );
+						area2.setText( aes.decryptBase64( area1.getText() ) );
 					} catch( Exception e ) {
-						// todo: replace with dialogBox
-						Window.alert( "Password format mistmatch:\n" + e.getMessage() );
-						return;
+						infoBox.showInfo( Strings.inst.errorDecrypting() );
 					}
-
-					area2.setText( aes.decryptBase64( area1.getText() ) );
 				}
 				else if( event.getSource() == clearButton )
 				{
-					passwordTextBox.setText( "" );
+					passwordTextBox1.setText( "" );
+					passwordTextBox2.setText( "" );
 					area1.setText( "" );
 					area2.setText( "" );
 				}
@@ -119,6 +220,8 @@ public class GWTCipher implements EntryPoint {
 		};
 
 		Handlers handler = new Handlers();
+		passwordTextBox1.addChangeHandler( handler );
+		passwordTextBox2.addChangeHandler( handler );
 		clearButton.addClickHandler( handler );
 		encryptButton.addClickHandler( handler );
 		decryptButton.addClickHandler( handler );
